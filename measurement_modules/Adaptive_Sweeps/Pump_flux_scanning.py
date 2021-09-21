84c26db41a075c0bfe42dd6aaa6eaa1121d61b3b
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2021-09-21 17:14:05 -0400

    small bugfixes

--- measurement_modules/Adaptive_Sweeps/Pump_flux_scanning.py
+++ measurement_modules/Adaptive_Sweeps/Pump_flux_scanning.py
@@ -14,27 +14,30 @@ from dataclasses import dataclass
 
 @dataclass
 class PS_dc: 
-    VNA: object
-    VNA_pstart: float
-    VNA_pstop: float
+    datadir: str = ''
+    name: str = ''
+    VNA: object = None
+    VNA_pstart: float = None
+    VNA_pstop: float = None
     VNA_ppoints: int = 100
     VNA_avgs: int = 20
     VNA_att: int = 0
-    VNA_detuning = 0.1e6
+    VNA_detuning: float = 0.1e6
     
-    Gen: object
+    Gen: object = None
     Gen_pstart: float = -20
     Gen_pstop: float = 20
     Gen_ppoints: int = 80
     Gen_detuning: float = 0
+    Gen_att: int = 0
     
-    CS: object
-    c_start: float
-    c_stop: float
-    c_points: int
-    c_ramp_rate: None
+    CS: object = None
+    c_start: float = None
+    c_stop: float = None
+    c_points: int = None
+    c_ramp_rate: object = None
     
-    fs_fit_path: str
+    fs_fit_path: str = ''
     
     def configure(self):
         #calculate the resonant_frequencies at each current using the fs_fit
@@ -44,19 +47,20 @@ class PS_dc:
         self.res_freqs = self.fs_fit_func(self.currents)
         self.Gen_freqs = 2*(self.res_freqs+self.Gen_detuning)
         self.VNA_cw_freqs = self.Gen_freqs/2+self.VNA_detuning
+        self.gen_powers = np.linspace(self.Gen_pstart, self.Gen_pstop, self.Gen_ppoints)
         
         #set VNA to power sweep mode
         self.VNA.sweep_type('POW')
         self.VNA.fcenter(self.res_freqs[0])
         self.VNA.math('NORM') #turning math off
-        self.VNA.power_starT(self.VNA_pstart)
+        self.VNA.power_start(self.VNA_pstart)
         self.VNA.power_stop(self.VNA_pstop)
         self.VNA.num_points(self.VNA_ppoints)
         self.VNA.avgnum(self.VNA_avgs)
         
     def ETA(self): 
         time_per_combo = self.VNA.sweep_time()
-        print(f"ETA: {np.round(time_per_combo*self.VNA.avgs*self.Gen_ppoints*self.c_points/60, 1)} minutes")
+        print(f"ETA: {np.round(time_per_combo*self.VNA.avgnum()*self.Gen_ppoints*self.c_points/60, 1)} minutes")
         
     def read_fs_data(self, fs_filepath, interpolation = 'linear'):
         ret = all_datadicts_from_hdf5(fs_filepath)
@@ -82,20 +86,19 @@ class Pump_flux_scan():
         
         self.datadict = dd.DataDict(
             current = dict(unit='A'),
-            vna_input_powers = dict(unit = 'dBm'),
+            vna_input_power = dict(unit = 'dBm'),
             vna_frequency = dict(unit='Hz'),
+            
             gen_frequency = dict(axes = ['current'], unit = 'Hz'),
             
-            undriven_vna_power = dict(axes=['current', 'vna_frequency'], unit = 'dBm'),
-            undriven_vna_phase = dict(axes=['current', 'vna_frequency'], unit = 'Degrees'), 
-            
-            driven_vna_power = dict(axes=['current', 'vna_frequency', 'gen_power'], unit = 'dBm'),
-            driven_vna_phase = dict(axes=['current', 'vna_frequency', 'gen_power'], unit = 'Degrees'), 
+            vna_return_power = dict(axes=['current', 'vna_frequency', 'vna_input_power'], unit = 'dBm'),
+            vna_phase = dict(axes=['current', 'vna_frequency', 'vna_input_power'], unit = 'Degrees'), 
+
         )
         
         self.datadict.validate()
         print('creating files')
-        self.writer = dds.DDH5Writer(self.datadir, self.datadict, name=self.name)
+        self.writer = dds.DDH5Writer(self.dc.datadir, self.datadict, name=self.dc.name)
         self.writer.__enter__()
         print('file created')
 
@@ -105,7 +108,7 @@ class Pump_flux_scan():
         vna_data = self.dc.VNA.average(self.dc.VNA_avgs)
         self.writer.add_data(
             current = self.dc.CS.current()*size_match, 
-            vna_input_powers = vna_pows, 
+            vna_input_power = vna_pows-self.dc.VNA_att, 
             vna_frequency = self.dc.VNA.fcenter()*size_match, 
             
             gen_frequency = self.dc.Gen.frequency()*size_match,
@@ -116,7 +119,7 @@ class Pump_flux_scan():
     
     def measure(self, debug = False):
         
-        for i, bias_current in enumerate(self.currents):
+        for i, bias_current in enumerate(self.dc.currents):
             
             self.dc.CS.change_current(bias_current, ramp_rate = self.dc.c_ramp_rate)
             self.dc.VNA.fcenter(self.dc.VNA_cw_freqs[i])
@@ -125,7 +128,7 @@ class Pump_flux_scan():
                 self.dc.Gen.output_status(1)
                 self.save_data()
             if i%5 == 0: 
-                print(f'--------------------\nPROGRESS: {np.round((i+1)/self.c_points*100)} percent  complete\n---------------------')
+                print(f'--------------------\nPROGRESS: {np.round((i+1)/self.dc.c_points*100)} percent  complete\n---------------------')
         print("Sweep, completed")
         self.writer.file.close()
             
