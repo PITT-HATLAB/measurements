751bf33c53d7c954bba997fb304089c07fd39643
borismesits <31737797+borismesits@users.noreply.github.com>
2021-10-06 15:55:44 -0400

    fixed some imports

--- sdocs/source/conf.py
+++ sdocs/source/conf.py
@@ -61,6 +61,12 @@ exclude_patterns = []
 #
 html_theme = 'classic'
 
+html_theme_options = {
+    "bgcolor": "ivory",
+    "sidebarbgcolor":"dimgray"
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
