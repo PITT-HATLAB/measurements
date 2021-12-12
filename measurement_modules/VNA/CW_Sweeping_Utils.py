7bd1e45dfdb4f1f5e1589b2909dbe2dc5776d03c
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2021-12-12 15:16:22 -0500

    Finished CW_Sweeps class
    needs power sweep implementation for dynamic range still. Can technically do a course sweep with 0-span but should do it right

--- measurement_modules/VNA/CW_Sweeping_Utils.py
+++ measurement_modules/VNA/CW_Sweeping_Utils.py
@@ -18,9 +18,9 @@ from itertools import product
 
 class CW_sweep(): 
     """
-    One class to unify all non-adaptive CW measurements"
+    One class to unify all non-adaptive CW measurements
+    e.g. gen power, gen frequency, vna power, vna_frequency sweeps
     - add independent parameters dynamically
-    - calibrate automatically to stored IO line attenuations
     """
     def __init__(self, 
                  name,
@@ -29,13 +29,11 @@ class CW_sweep():
                  SA_inst = None, 
                  Gen_arr = []): 
         
-        self.filename_prepend = name+'_'
+        self.sweep_name = name+'_'+mode
         self.mode = mode
         
-        if mode == 'VNA': 
-            self.VNA_inst = VNA_inst
-        if mode == 'SA': 
-            self.SA_inst = SA_inst
+        self.VNA_inst = VNA_inst
+        self.SA_inst = SA_inst
         
         self.Gen_inst_arr = Gen_arr
         self.ind_par_dict_arr = []
@@ -48,46 +46,12 @@ class CW_sweep():
         '''
         self.ind_par_dict_arr.append(ind_par_dict)
         self.is_ind_par_set = True
-    
-    def filename_func(self, val_dict):
-        '''
-        takes in a dictionary where the parameters are the keys, and the vals are the setpoints
-        eg {SigGen.power: dict('name' = str, 'val' = float))}
-        '''
-        filename = self.filename_prepend
-        # for name, val in val_dict.items(): 
-        #     filename += (name+'_')
-        #     filename += (str(np.round(val['val'], 3))+'_'+val['parameter'].unit + '_')
-        return filename
-    
-    def pre_measurement_operation(self): 
-        '''
-        function that handles preparation for the experiment
-        '''
-        # print(f'Setting {name} to {val["val"]} {val["parameter"].unit}\nvia {val["parameter"].name}')
         
-        #TODO: this sucks! Make it better, general try except loops are dangerous
-        for Gen in self.Gen_inst_arr: 
-            try:     
-                Gen.output_status(1)
-            except:
-                pass
-            
-
-    def post_measurement_operation(self, i): 
-        for Gen in self.Gen_inst_arr:
-            try: 
-                Gen.output_status(0)
-            except: 
-                pass
-        print(f"\nMeasurement {i+1} out of {np.shape(list(self.setpoint_arr))[0]} completed\n")
-        
-    def make_datadict(self):
+    def make_datadict(self, mode):
         '''
         use the independent variables supplied by CW_sweep.add_independent_variables 
         
         and assume the dependent variables are either (vna phase, vna power),  Spectrum PSD, or both
-        
         '''
         
         print(self.ind_par_dict_arr)
@@ -97,23 +61,48 @@ class CW_sweep():
         for ind_par_dict in self.ind_par_dict_arr: 
             dd_ind_var_dict[ind_par_dict['name']] = dict(unit = ind_par_dict['parameter'].unit)
             
-        if self.mode == 'VNA': 
+        if mode == 'VNA': 
             axes_arr.append('vna_frequency')
             dd_ind_var_dict['vna_frequency'] = dict(unit = 'Hz')
             dd_dep_var_dict = dict(vna_phase = dict(axes = axes_arr, unit = 'Rad'),
                                    vna_power = dict(axes = axes_arr, unit = 'dB')                                   
                                    )
-        if self.mode == 'SA': 
+        if mode == 'SA': 
             axes_arr.append('spec_frequency')
             dd_ind_var_dict['spec_frequency'] = dict(unit = 'Hz')
-            dd_dep_var_dict = dict(spec_power = dict(axes = axes_arr, unit = 'dB')                                   
-                                   )
+            dd_dep_var_dict = dict(spec_power = dict(axes = axes_arr, unit = 'dBm'))
             
         assert self.is_ind_par_set == True
         
-        return  dd.DataDict(**(dd_ind_var_dict | dd_dep_var_dict))
+        return  dd.DataDict(**(dd_ind_var_dict | dd_dep_var_dict)) 
     
-    def sweep(self, DATADIR, debug = True, avgnum = 10):
+    def pre_measurement_operation(self):
+        '''
+        function that handles preparation for that particular setpoint of the experiment
+        Adding dependency on the setpoint dictionary lets you make adjustments to the measurement, 
+        e.g. changing the vna window for a Duffing Test
+        
+        But this should NOT be done in this class, you should create another class that subclasses this 
+        one (CW_Sweeps), then write a method that overrides this one
+        '''
+        # print(f'Setting {name} to {val["val"]} {val["parameter"].unit}\nvia {val["parameter"].name}')
+        
+        #TODO: this sucks! Make it better, general try except loops are dangerous
+        [Gen.output_status(1) if Gen is not None else '' for Gen in self.Gen_inst_arr]
+        print("Current parameters: ")
+        [print(key,': ', val) for key, val in self.setpoint_dict.items()]
+
+    def post_measurement_operation(self, i): 
+        '''
+        very similar to pre_measurement_operation, this can be overridden 
+        in a subclass to do smarter sweeps
+        '''
+        print(f"\nMeasurement {i+1} out of {np.shape(list(self.setpoint_arr))[0]} completed\n")
+        
+        if i+1 == np.shape(list(self.setpoint_arr))[0]: #the sweep is done
+            [Gen.output_status(0) if Gen is not None else '' for Gen in self.Gen_inst_arr]
+    
+    def sweep(self, DATADIR, debug = True, VNA_avgnum = 10, SA_avgnum = 200):
         
         if not self.is_ind_par_set: 
             raise Exception("Independent parameter not yet set. Run set_independent_parameter method")
@@ -123,21 +112,21 @@ class CW_sweep():
         ind_par_parameters = []
         ind_par_vals = []
         for ind_par_dict in self.ind_par_dict_arr: 
-            for name, info_dict in ind_par_dict.items(): 
-                ind_par_names.append(name)
-                ind_par_parameters.append(ind_par_dict[name]['parameter'])
-                ind_par_vals.append(ind_par_dict[name]['vals'])
-
-        ####################### Set up the datadict
-        self.datadict =self.make_datadict()
-        
-        ####################### Prepare for taking data
-        self.pre_measurement_operation()
-        filename = self.filename_func(self.setpoint_dict)
-        self.writer = dds.DDH5Writer(DATADIR, self.datadict, name=filename)
-        self.writer.__enter__()
-        self.savepath = self.writer.file_path
-        
+            ind_par_names.append(ind_par_dict['name'])
+            ind_par_parameters.append(ind_par_dict['parameter'])
+            ind_par_vals.append(ind_par_dict['vals'])
+    
+        if self.mode == 'VNA' or self.mode == 'both': 
+            self.vna_datadict =self.make_datadict(mode = 'VNA')
+            self.vna_writer = dds.DDH5Writer(DATADIR, self.vna_datadict, name=self.sweep_name+'_VNA')
+            self.vna_writer.__enter__()
+            self.vna_savepath = self.vna_writer.file_path
+            
+        if self.mode == 'SA' or self.mode == 'both': 
+            self.sa_datadict =self.make_datadict(mode = 'SA')
+            self.sa_writer = dds.DDH5Writer(DATADIR, self.sa_datadict, name=self.sweep_name+'_SA')
+            self.sa_writer.__enter__()
+            self.sa_savepath = self.sa_writer.file_path
         '''
         each combination of independent variable values creates a 
         "setpoint" which defines a place in parameter space where the 
@@ -153,30 +142,60 @@ class CW_sweep():
                 self.setpoint_dict[ind_par_names[j]] = dict(parameter = ind_par_parameters[j], 
                                                        val = values[j])
 
+            ####################### Prepare for taking data
+            self.pre_measurement_operation()
             #setting independent parameter values
             for name, val in self.setpoint_dict.items(): 
                 val['parameter'](val['val'])
+
+            #taking data with VNA, SA, or both
             
-            if self.mode == 'VNA': 
+            if self.mode == 'VNA' or self.mode == 'both': 
+                if debug: print("\nVNA measuring\n")
                 vna_freqs = self.VNA_inst.getSweepData() #this could also be a power... fuck
-                vna_data = self.VNA_inst.average(avgnum)
+                vna_data = self.VNA_inst.average(VNA_avgnum)
                 vna_power = vna_data[0]
                 vna_phase = vna_data[1]
-                d = np.shape(vna_data)[0]
+                d = np.shape(vna_data)[1]
     
-                writer_dict = {}
+                vna_writer_dict = {}
                 
                 for ind_par_dict in self.ind_par_dict_arr: 
-                    writer_dict[ind_par_dict['name']] = ind_par_dict['parameter']()*np.ones(d) #asks the instrument for an update, resize for plottr
+                    vna_writer_dict[ind_par_dict['name']] = ind_par_dict['parameter']()*np.ones(d) #asks the instrument for an update, resize for plottr
                 
                 #I need to find a way to incorporate power sweeps into this too. for now, just frequency
-                writer_dict['vna_frequency'] = vna_freqs
+                vna_writer_dict['vna_frequency'] = vna_freqs
                 
                 #add the dependent data...
-                writer_dict['vna_power'] = vna_power
-                writer_dict['vna_phase'] = vna_phase
+                vna_writer_dict['vna_power'] = vna_power
+                vna_writer_dict['vna_phase'] = vna_phase
                 
-                self.writer.add_data(**writer_dict)
+                self.vna_writer.add_data(**vna_writer_dict)
                 
+            if self.mode == 'SA' or self.mode == 'both': 
+                if debug: print("\nSA measuring\n")
+                sa_data = self.SA_inst.get_data(count = SA_avgnum)
+                sa_freqs = sa_data[:,  0]
+                sa_power = sa_data[:,  1]
+                d = np.size(sa_freqs)
+    
+                sa_writer_dict = {}
+                
+                for ind_par_dict in self.ind_par_dict_arr: 
+                    sa_writer_dict[ind_par_dict['name']] = ind_par_dict['parameter']()*np.ones(d) #asks the instrument for an update, resize for plottr
+                
+                #I need to find a way to incorporate power sweeps into this too. for now, just frequency
+                sa_writer_dict['spec_frequency'] = sa_freqs
+                
+                #add the dependent data...
+                sa_writer_dict['spec_power'] = sa_power
+                
+                self.sa_writer.add_data(**sa_writer_dict)
+
             self.post_measurement_operation(i)
-        self.writer.__exit__()
+        
+        if self.mode == 'SA' or self.mode == 'both': 
+            self.sa_writer.file.close()
+        
+        if self.mode == 'VNA' or self.mode == 'both': 
+            self.vna_writer.file.close()
--- measurement_modules/VNA/CW_sweep_testing.py
+++ measurement_modules/VNA/CW_sweep_testing.py
@@ -12,7 +12,7 @@ import measurement_modules.VNA.CW_Sweeping_Utils as CW
 import logging
 from qcodes.instrument.parameter import Parameter
 from plottr.apps.autoplot import main
-
+#%%
 class test_parameter(Parameter): 
     def __init__(self, name):
         # only name is required
@@ -26,33 +26,58 @@ class test_parameter(Parameter):
 
     def set_raw(self, val):
         self._value = val
-        
-pVNA = None
-CXA = None
-SigGen = None
+
+class wrapped_parameter(Parameter): 
+    def __init__(self, name, inst_parameter): 
+        super().__init__(name)
+        self.value = inst_parameter()
+        self.unit = inst_parameter.unit
+        self._inst_par = inst_parameter
+    def get_raw(self): 
+        self._inst_par()
+    def set_raw(self, val): 
+        self._inst_par(val)
+
 #%%
-DATADIR = r''
-name = 'test'
+# DATADIR = r''
+# name = 'test'
 
-tp1_vals = np.linspace(0, 1, 100)
-tp2_vals = np.linspace(1, 2, 100)
+# tp1_vals = np.linspace(0, 1, 100)
+# tp2_vals = np.linspace(1, 2, 100)
 
-CWSWP = CW.CW_sweep(name, "VNA")
+# CWSWP = CW.CW_sweep(name, "VNA")
 
-test_parameter_1 = test_parameter('ind_var_1')
-test_parameter_2 = test_parameter('ind_var_2')
-#ind_par_dict{name: dict(parameter = actual_parameter_class, vals = [np_val_arr])}
-# amp_dict = dict(Amp = dict(parameter = SigGen.output_status, vals = np.array([0,1])))
-test_par_1 = dict(name = 'ind_var_1_name', parameter = test_parameter_1, vals = tp1_vals)
-test_par_2 = dict(name = 'ind_var_2_name', parameter = test_parameter_2, vals = tp2_vals)
-# 
-# CWSWP.add_independent_parameter(amp_dict)
-CWSWP.add_independent_parameter(test_par_1)
-CWSWP.add_independent_parameter(test_par_2)
+# test_parameter_1 = test_parameter('ind_var_1')
+# test_parameter_2 = test_parameter('ind_var_2')
+# #ind_par_dict{name: dict(parameter = actual_parameter_class, vals = [np_val_arr])}
+# # amp_dict = dict(Amp = dict(parameter = SigGen.output_status, vals = np.array([0,1])))
+# test_par_1 = dict(name = 'ind_var_1_name', parameter = test_parameter_1, vals = tp1_vals)
+# test_par_2 = dict(name = 'ind_var_2_name', parameter = test_parameter_2, vals = tp2_vals)
+# # 
+# # CWSWP.add_independent_parameter(amp_dict)
+# CWSWP.add_independent_parameter(test_par_1)
+# CWSWP.add_independent_parameter(test_par_2)
 
-ddtest = CWSWP.make_datadict()
+# ddtest = CWSWP.make_datadict()
 
-print(ddtest)
+DATADIR = r'Z:\Data\testing'
+
+name = 'CW_Sweeps_VNA_and_SA_test'
+
+CWSWP = CW.CW_sweep(name, "both", VNA_inst = pVNA, SA_inst = CXA, Gen_arr = [SigGen])
+
+vna_power_dict = dict(name = 'vna_input_power', parameter = pVNA.power, vals = np.array([-30, -25, -20]))
+pump_power_dict = dict(name = 'pump_power', parameter = SigGen.power, vals = np.linspace(-10, 0, 6))
+
+# CWSWP.add_independent_parameter(vna_power_dict)
+CWSWP.add_independent_parameter(pump_power_dict)
+
 #%%
 # V(2)
-CWSWP.sweep(DATADIR, debug = False)
+CWSWP.sweep(DATADIR, debug = True)
+
+
+
+
+
+
