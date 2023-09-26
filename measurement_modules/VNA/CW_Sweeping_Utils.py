1c66b33e5f8fc278d4778d4a0c5b0e9340a800f2
Chao <chz78@pitt.edu>
2023-09-26 15:54:42 -0400

    Added IPN sweeping parameter
    Added parameter for iterating through IMD peaks

--- measurement_modules/VNA/CW_Sweeping_Utils.py
+++ measurement_modules/VNA/CW_Sweeping_Utils.py
@@ -47,7 +47,8 @@ class amplifier_bias(Parameter):
     def __init__(self, current_par, generator_freq_par, fs_fit_filepath, gen_offset = 500e6, gen_factor = 1, norm = 0, VNA_inst = None): 
         super().__init__('bias_current')
         self._current_par = current_par
-        self.fs_fit_func = self.read_fs_data(fs_fit_filepath)
+        self.fs_fit_filepath = fs_fit_filepath
+        self.fs_fit_func = self.read_fs_data(self.fs_fit_filepath)
         self._generator_freq_par = generator_freq_par
         self._gen_offset = gen_offset
         self._gen_factor = gen_factor
@@ -58,7 +59,7 @@ class amplifier_bias(Parameter):
         ret = all_datadicts_from_hdf5(fs_filepath)
         res_freqs = ret['data'].extract('base_resonant_frequency').data_vals('base_resonant_frequency')
         currents = ret['data'].extract('base_resonant_frequency').data_vals('current')
-        fs_fit_func = interp1d(currents, res_freqs, interpolation)
+        fs_fit_func = interp1d(currents, res_freqs, interpolation, fill_value="extrapolate")
         return fs_fit_func
     
     def get_raw(self): 
@@ -72,7 +73,9 @@ class amplifier_bias(Parameter):
     
     def preview_range(self, currents, show_pump = False): 
         fig, ax = plt.subplots(1,1)
-        
+
+        print(currents)
+
         ax.plot(currents*1000, self.fs_fit_func(currents), label = 'Amp_res')
         if show_pump: 
             ax.plot(currents*1000, (self.fs_fit_func(currents)*self._gen_factor+self._gen_offset)/self._gen_factor, '--', label = f'Generator/{self._gen_factor}')
@@ -91,7 +94,7 @@ class setpoint_iterator(Parameter):
     The actual generator + current bias settings will have to be backed out from teh same file
     that this parameter is working from
     '''
-    def __init__(self, current_par, generator_freq_par, generator_power_par, vna_fcenter_parameter, vna_fspan_parameter, setpoint_fp, fs_fit_filepath, vna_freq_range = 100e6, norm = 0):
+    def __init__(self, current_par, generator_freq_par, generator_power_par, vna_fcenter_parameter, vna_fspan_parameter, setpoint_fp, fs_fit_filepath, vna_freq_range = 100e6, norm = 0, vna_offset = 0):
         super().__init__('bias_current')
         self._current_par = current_par
         self.fs_fit_func = self.read_fs_data(fs_fit_filepath)
@@ -102,6 +105,7 @@ class setpoint_iterator(Parameter):
         self.read_setpoint_file(setpoint_fp)
         self._vna_freq_range = vna_freq_range
         self._norm = norm
+        self._active_setpoint_num = 0
 
     def read_fs_data(self, fs_filepath, interpolation = 'linear'):
         ret = all_datadicts_from_hdf5(fs_filepath)
@@ -121,20 +125,121 @@ class setpoint_iterator(Parameter):
         return np.max(self.setpoint_num)
 
     def get_raw(self): #not sure which this should return...
-        return self._current_par()
+        return self._active_setpoint_num
 
     def set_raw(self, setpoint_num_val):
         setpoint_filt = self.setpoint_num == setpoint_num_val
         setpoint_current = self.setpoint_current[setpoint_filt][0] #there should only be one element
         setpoint_pump_freq = self.setpoint_pump_freq[setpoint_filt][0]
         setpoint_pump_pwr = self.setpoint_pump_pwr[setpoint_filt][0]
-        self._vna_fcenter_parameter(setpoint_pump_freq/2)
+        print(setpoint_current, setpoint_pump_freq/1e9, setpoint_pump_pwr)
+        self._vna_fcenter_parameter(setpoint_pump_freq/2+vna_offset)
         self._vna_fspan_parameter(self._vna_freq_range)
         self._current_par(setpoint_current)
         self._generator_freq_par(setpoint_pump_freq)
+        if setpoint_pump_pwr>19:
+            setpoint_pump_pwr = 19
         self._generator_power_par(setpoint_pump_pwr)
+        self._active_setpoint_num = setpoint_num_val
         if self._norm:
             self.VNA_inst.renormalize(3 * self.VNA_inst.avgnum())
+
+class setpoint_iterator_VNA_and_SA(Parameter):
+    '''
+    Purpose: take a series of setpoints for an amplifier (current, pump frequency, pump power)
+    and check their saturation powers by stepping up the VNA power over the entire bandwidth
+
+    This parameter will go through setpoints and take care of the instrument settings accordingly
+    the final sweep will only have setpoints+vna input power + vna frequencies as independent variables.
+    The actual generator + current bias settings will have to be backed out from teh same file
+    that this parameter is working from
+    '''
+    def __init__(self, current_par, generator_freq_par, generator_power_par, vna_fcenter_parameter, vna_fspan_parameter, MXA_fcenter_parameter, MXA_fspan_parameter, setpoint_fp, fs_fit_filepath, vna_freq_range = 100e6, SA_freq_range = 10e6, norm = 0, vna_offset = 0, SA_offset = 0):
+        super().__init__('bias_current')
+        self._current_par = current_par
+        self.fs_fit_func = self.read_fs_data(fs_fit_filepath)
+        self._generator_freq_par = generator_freq_par
+        self._generator_power_par = generator_power_par
+        self._vna_fcenter_parameter = vna_fcenter_parameter
+        self._vna_fspan_parameter = vna_fspan_parameter
+        self._mxa_fcenter_parameter = MXA_fcenter_parameter
+        self._mxa_fspan_parameter = MXA_fspan_parameter
+        self.read_setpoint_file(setpoint_fp)
+        self._vna_freq_range = vna_freq_range
+        self._sa_freq_range = SA_freq_range
+        self._vna_offset = vna_offset
+        self._sa_offset = SA_offset
+        self._norm = norm
+        self._active_setpoint_num = 0
+
+    def read_fs_data(self, fs_filepath, interpolation = 'linear'):
+        ret = all_datadicts_from_hdf5(fs_filepath)
+        res_freqs = ret['data'].extract('base_resonant_frequency').data_vals('base_resonant_frequency')
+        currents = ret['data'].extract('base_resonant_frequency').data_vals('current')
+        fs_fit_func = interp1d(currents, res_freqs, interpolation)
+        return fs_fit_func
+
+    def read_setpoint_file(self, setpoint_fp):
+        setpoint_data = all_datadicts_from_hdf5(setpoint_fp)['data']
+        self.setpoint_num = setpoint_data['setpoint_num']['values']
+        self.setpoint_current = setpoint_data['current']['values']
+        self.setpoint_pump_freq = setpoint_data['pump_freq']['values']
+        self.setpoint_pump_pwr = setpoint_data['pump_pwr']['values']
+
+    def total_setpoint_number(self):
+        return np.max(self.setpoint_num)
+
+    def get_raw(self): #not sure which this should return...
+        return self._active_setpoint_num
+
+    def set_raw(self, setpoint_num_val):
+        setpoint_filt = self.setpoint_num == setpoint_num_val
+        setpoint_current = self.setpoint_current[setpoint_filt][0] #there should only be one element
+        setpoint_pump_freq = self.setpoint_pump_freq[setpoint_filt][0]
+        setpoint_pump_pwr = self.setpoint_pump_pwr[setpoint_filt][0]
+        print(setpoint_current, setpoint_pump_freq/1e9, setpoint_pump_pwr)
+        self._vna_fcenter_parameter(setpoint_pump_freq/2+self._vna_offset)
+        self._vna_fspan_parameter(self._vna_freq_range)
+        self._mxa_fcenter_parameter(setpoint_pump_freq/2+self._sa_offset)
+        self._mxa_fspan_parameter(self._sa_freq_range)
+        self._current_par(setpoint_current)
+        self._generator_freq_par(setpoint_pump_freq)
+        if setpoint_pump_pwr>19:
+            setpoint_pump_pwr = 19
+        self._generator_power_par(setpoint_pump_pwr)
+        self._active_setpoint_num = setpoint_num_val
+        if self._norm:
+            self.VNA_inst.renormalize(3 * self.VNA_inst.avgnum())
+
+class IPN_peak_parameter(Parameter):
+    """
+    The setpoint iterator is enough to get us from bias point to bias point. However, with a broad bandwidth and
+    therefore a broad RBW, the spectrum analyzer noise floor is too high to detect intermodulation at the low
+    powers that we need
+
+    Therefore, since we know where the peaks will show up (either signal to pump detuning or signal1 to signal2 detuning)
+    we know where the peaks would show up, so we can make this parameter iterate through frequency windows if interest.
+
+    The peak number that this will call will be an integer. Technically any integer, but it will take this integer, multiply it by the detuning,
+    then move to the pump/2 frequency minus that number.
+    """
+    def __init__(self, sa_fcenter_parameter, detuning, pump_frequency_parameter):
+        '''
+        It's importnat that you get the order of operations right here. The setpoint is set, and then this parameter acts.
+        It will use the pump frequency parameter combined with detunning to know where the tones will show up.
+        What makes this extra nice is that you can check for what's happening at the pump/2 frequency itself by inputting 0
+        :param detuning:
+        '''
+        self._pump_freq_param = pump_frequency_parameter
+        self._detuning = detuning
+        self._current_peak_number
+        self._sa_fcenter_parameter = sa_fcenter_parameter
+        def get_raw(self):
+            return self._current_peak_number
+
+        def set_raw(self, peak_num):
+            fcenter = self._pump_freq_param()/2
+            self._sa_fcenter_parameter(fcenter+peak_num*self._detuning)
 
 
 
@@ -145,10 +250,11 @@ class generator_detuning(Parameter):
     Now that I can track resonant frequency, I need a seperate parameter that can also detune the generator from a setpoint. This is different than the previous parameter because it needs to output the generator frequency from the get_raw function, NOT a current
     '''
     
-    def __init__(self, generator_freq_par):
+    def __init__(self, generator_freq_par, amp_par):
         super().__init__('gen_detuning')
         self._generator_freq_par = generator_freq_par
         self.last_val = 0
+        self.amp_par = amp_par
         
     def get_raw(self): 
         # return self._generator_freq_par()
@@ -156,7 +262,7 @@ class generator_detuning(Parameter):
     
     def set_raw(self, val):
         self.last_val = val
-        self._generator_freq_par(self._generator_freq_par()+val)
+        self._generator_freq_par(2*self.amp_par.fs_fit_func(self.amp_par())+val)
         
 class Time(Parameter):
     '''
@@ -282,16 +388,18 @@ class CW_sweep():
             self.VNA_inst.num_points(points)
         elif self.VNA_mode == 'WRONG_INPUT': 
             raise Exception('Wrong user input, either "POW" or "FREQ"')
-    def setup_SA(self, start, stop):
+    def setup_SA(self, start, stop, pt_num = 1001):
         '''
         Function for setting up the SA
         '''
         try:
             self.SA_inst.fstart(start)
             self.SA_inst.fstop(stop)
+            self.SA_inst.num_points(pt_num)
         except:
             self.SA_inst.frequency_start(start)
             self.SA_inst.frequency_stop(stop)
+            self.SA_inst.num_points(pt_num)
 
         
     def VNA_parameter_name(self): 
@@ -430,7 +538,7 @@ class CW_sweep():
         '''
         
         self.setpoint_arr = list(product(*ind_par_vals))
-
+        self.VNA_inst.rfout(1)
         for i, values in enumerate(self.setpoint_arr): 
             self.setpoint_dict = {}
             for j in range(np.size(values)):
@@ -468,6 +576,9 @@ class CW_sweep():
                 vna_power = vna_data[0]
                 vna_phase = vna_data[1]
                 d = np.shape(vna_data)[1]
+                print("DEBUG", np.shape(vna_data)[0], d)
+                if np.shape(vna_ind_var)[0]!=d:
+                    vna_ind_var=np.ones(d)*vna_ind_var.copy()
     
                 vna_writer_dict = {}
                 
@@ -486,7 +597,8 @@ class CW_sweep():
                 
             if self.mode == 'SA' or self.mode == 'both': 
                 if debug: print("\nSA measuring\n")
-
+                self.SA_inst.average()
+                if debug: print("SA data cleared")
                 sa_data = self.SA_inst.get_data(count = SA_avgnum)
                 sa_freqs = sa_data[:,  0]
                 sa_power = sa_data[:,  1]
@@ -507,7 +619,7 @@ class CW_sweep():
                 self.sa_writer.add_data(**sa_writer_dict)
 
             self.post_measurement_operation(i)
-        
+        self.VNA_inst.rfout(0)
         if self.mode == 'SA' or self.mode == 'both':
             if not new_plottr:
                 self.sa_writer.file.close()
